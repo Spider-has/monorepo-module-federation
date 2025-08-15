edd1060f6481e52a42dfa38d599e65c36a33f7c8
Spider-has <romanh998@gmail.com>
2025-08-15 23:09:41 +0300

    init.ts

--- services/real-time-chat/src/app/firebase/init.ts
+++ services/real-time-chat/src/app/firebase/init.ts
@@ -2,8 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log(__PLATFORM__);
-
 const firebaseConfig = {
   apiKey: process.env.firebase_api_key,
   authDomain: process.env.firebase_auth_domain,
@@ -12,8 +10,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.firebase_messaging_sender_id,
   appId: process.env.firebase_app_id,
 };
-
-console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
